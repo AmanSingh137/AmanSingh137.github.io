ed74dc7d55c910966d96f09f6c23f224502f7edf
AmanSingh137 <65694984+AmanSingh137@users.noreply.github.com>
2021-08-06 17:41:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const liner = document.querySelector('.liner');
 const headerRight = document.querySelector('.header-right');
 const headerRighter = document.querySelector('.header-right a');
@@ -6,3 +5,14 @@ const headerRighter = document.querySelector('.header-right a');
 liner.addEventListener("click", ()=>{
     headerRight.classList.toggle("open");
 })
+
+navigator.getBattery().then(function(battery){
+    
+    level();
+    function level(){
+        var level=battery.level*100+"%";
+        document.getElementById("level").innerHTML=level;
+
+    }
+});
+
